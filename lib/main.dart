8498f2aab513ae4c35d43497e722fcab4ba13905
Habiba Ashraf <MY_NAME@example.com>
2021-04-06 00:02:37 +0200

    batates

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
         ),
         initialRoute: '/',
         routes: {
-          '/': (context) => Settings(),
+          '/': (context) => Profile(),
         });
   }
 }
