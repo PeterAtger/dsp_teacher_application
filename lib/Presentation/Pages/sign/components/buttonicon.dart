e77de0d18f04872b438c72db29dda7eb96b237a3
Peter_Atef <ando9910@gmail.com>
2021-07-21 07:36:35 +0200

    Fixed button text overflow for Doba

--- lib/Presentation/Pages/sign/components/buttonicon.dart
+++ lib/Presentation/Pages/sign/components/buttonicon.dart
@@ -1,6 +1,7 @@
 import 'package:dsp_teacher_application/Presentation/Theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class ButtonIcon extends StatelessWidget {
   const ButtonIcon({
@@ -26,30 +27,39 @@ class ButtonIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: onButtonPress,
-      icon: Padding(
-        padding: const EdgeInsets.only(top: 16, left: 0, right: 0, bottom: 16),
-        child: Container(
-          width: 24,
-          height: 24,
-          child: SvgPicture.asset(
-            'lib/Presentation/Images/$icon.svg',
-          ),
+    return Container(
+      height: 48,
+      child: ElevatedButton(
+        onPressed: onButtonPress,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0),
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SvgPicture.asset(
+              'lib/Presentation/Images/$icon.svg',
+              width: 16,
+              height: 16,
+            ),
+            Expanded(
+              child: AutoSizeText(
+                text,
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                minFontSize: 1,
+                maxFontSize: 15,
+                style: AppFonts.button.copyWith(
+                  color: textcolor,
+                ),
+              ),
+            ),
+          ]),
         ),
-      ),
-      label: Text(
-        text,
-        style: AppFonts.button.copyWith(
-          color: textcolor,
-        ),
-      ),
-      style: ButtonStyle(
-        overlayColor: MaterialStateProperty.all(AppColors.cDarkGrey[500]),
-        backgroundColor: MaterialStateProperty.all(buttoncolor),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(32.0),
+        style: ButtonStyle(
+          overlayColor: MaterialStateProperty.all(AppColors.cDarkGrey[500]),
+          backgroundColor: MaterialStateProperty.all(buttoncolor),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(32.0),
+            ),
           ),
         ),
       ),
